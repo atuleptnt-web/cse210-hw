5225ab34e7259b56f9d6cd8dc448a2f639928bb0
paultetteh <pnerquaye@byupathway.edu>
2026-03-15 00:44:10 +0000

    finished update

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,9 +5,47 @@ public class Program
     public static void Main(string[] args)
     {
         Journal journal = new Journal();
-        //Entry entry = new Entry("2026","Hello","Koko");
         PromptGenerator generator = new PromptGenerator();
 
-        Console.WriteLine("Welcome to the Journal Program!");
+        string choice = "";
+        while (choice != "5")
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal to file");
+            Console.WriteLine("4. Load journal from file");
+            Console.WriteLine("5. Quit");
+            Console.Write("Choose an option: ");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = generator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.ToShortDateString();
+                    Entry entry = new Entry(date, prompt, response);
+                    journal.AddEntry(entry);
+                    break;
+
+                case "2":
+                    journal.DisplayAll();
+                    break;
+
+                case "3":
+                    Console.Write("Enter filename: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveToFile(saveFile);
+                    break;
+
+                case "4":
+                    Console.Write("Enter filename: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
+                    break;
+            }
+        }
     }
 }
